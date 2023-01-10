4c515a78a88212ad4cc2ac1c32dcf99f2ce73170
nmdeni <mekadeni@mail.ru>
2023-01-10 21:06:10 +0300

    сделал первую версию проги

---
+++ version1/config.py
@@ -0,0 +1,17 @@
+users_list = [
+    {
+        'name':'Alex',
+        'sum': 1000,
+        'status' : True,
+    },
+    {
+        'name':'Neo',
+        'sum': 320,
+        'status' : False,
+    },
+    {
+        'name':'Dima',
+        'sum': 4500,
+        'status' : True,
+    },
+]
--- version1/main.py
+++ version1/main.py
@@ -1,12 +1,27 @@
-запуск программы
-запрос пароля
-......
+from config import users_list
 
-def main(password):
-    if password == "test":
-        print("шалом")
-        return 0
+# ОБРАБОТКА/ВЫВОД ДАННЫХ
+def main():
+    print("\nДобро пожаловать!\n")
+    try:
+        users = users_list
+        for user in users_list:
+            for key,val in user.items():
+                print(f"{key} -> {val}")
+
+            print("-"*20)
+    except:
+        return print("[INFO] ошибка данных")
+    finally:
+        # КОНЕЦ
+        return print("Конец программы")
+
+# ВВОД ПАРОЛЯ
+def password_check():
+    user_password = input("Введите пароль -> ")
+    if user_password == "qwerty": # ТУТ ПАРОЛЬ!!!!
+        return main()
     else:
-        print("хуйня")
-
-......
+        return password_check()
+# НАЧАЛО
+password_check()
